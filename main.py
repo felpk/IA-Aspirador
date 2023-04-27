0adbf17abfe92aee2db6fd06db05118f4ff4795f
felpk <mfrzbr@gmail.com>
2023-04-27 09:07:14 -0300

    implementação funcao aspirar

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ sujo = 1
 limpo = 0
 
 posicao = [1, 1]    #posicao inicial
-#orientacao = 'norte'   #orientacao inicial
+posicao_atual = posicao #posicao atual
 table = [
-    [(0, 0), sujo], [(0, 1), sujo], [(0, 2), limpo],
+    [(0, 0), sujo],  [(0, 1), sujo],  [(0, 2), limpo],
     [(1, 0), limpo], [(1, 1), limpo], [(1, 2), sujo],
-    [(2, 0), sujo], [(2, 1), limpo], [(2, 2), sujo]
+    [(2, 0), sujo],  [(2, 1), limpo], [(2, 2), sujo]
 ]
 
 def mover(direção, posição_atual):
-    if direção == 'frente':
+    if direção == 'norte':
         posição_atual[0] -= 1
         
     elif direção == 'direita':
@@ -19,7 +19,7 @@ def mover(direção, posição_atual):
     elif direção == 'esquerda':
         posição_atual[1] -= 1
     
-    elif direção == 'tras':
+    elif direção == 'sul':
         posição_atual[0] += 1
         
         
@@ -31,34 +31,41 @@ def mover(direção, posição_atual):
     
     return posição_atual
 
-
-teste = ['frente',  #0, 1
-         'frente',  #0, 1
-         'direita', #0, 2
-         'tras',    #1, 2
-         'tras',    #2, 2
-         'esquerda', #2, 1
-         'frente',  #1, 1
-         'esquerda']#1, 0
-aspirador = posicao
-
-def aspirar():
+def aspirar():  # Funcao Aspirar Funcionando
     global i
     global j 
-    i = posicao[0]
-    j = posicao[1]
+    i = posicao_atual[0]
+    j = posicao_atual[1]
 
     if table[i][j] == limpo:
-        print(f'Espaço [{i, j}] já está limpo!\n')
+        return(f'Espaço [{i, j}] já está limpo!\n')
 
     elif table[i][j] == sujo:
         table[i][j] = limpo
-        print(f'Espaço [{i, j}] limpo!\n')
+        return(f'Espaço [{i, j}] limpo!\n')
+
+# for i, pos in enumerate(teste):
+#     aspirador = mover(pos, aspirador)
+#     print(aspirador)
+
+def imprimir_ambiente():    # Imprimir matriz ambiente onde 0 é limpo e 1 é sujo Funcionando
+    print()
+    for i in range(3):
+        for j in range(3):
+            index = i * 3 + j
+            print(table[index][1], end=' ')
+        print()
+    print()
 
 
+def busca_ambiente():  # Verifica redondezas do agente para identificar sujeiras 
+    ####
+    return 0
 
+print("\nASPIRADOR")
 
-for i, pos in enumerate(teste):
-    aspirador = mover(pos, aspirador)
-    print(aspirador)
+imprimir_ambiente()
 
+print("\nPosicao atual: ", busca_ambiente())
+
+imprimir_ambiente()
