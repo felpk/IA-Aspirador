b322f2be1e238c8933195359905b1eef8a52f033
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2023-04-27 10:48:00 -0300

    Atualização da função mover

--- main.py
+++ main.py
@@ -3,39 +3,48 @@ limpo = 0
 i = 1
 j = 1
 posicao = [1, 1]    #posicao inicial
+#orientacao = 'norte'   #orientacao inicial
 table = [
-    [(1,1), sujo], [(1,2), sujo], [(1,3), limpo],
-    [(2,1), limpo], [(2,2), limpo], [(2,3), sujo],
-    [(3,3), sujo], [(3,2), limpo], [(3,3), sujo]
+    [(0, 0), sujo], [(0, 1), sujo], [(0, 2), limpo],
+    [(1, 0), limpo], [(1, 1), limpo], [(1, 2), sujo],
+    [(2, 0), sujo], [(2, 1), limpo], [(2, 2), sujo]
 ]
 
-orientacao = 'oeste'   #orientacao inicial
-
-def mover_frente():
-    if orientacao == 'norte':
-        posicao[0] -= 1
-
-    elif orientacao == 'leste':
-        posicao[1] += 1
-
-    elif orientacao == 'oeste':
-        posicao[1] -= 1
-
-    elif orientacao == 'sul':
-        posicao[0] += 1
-
-def girar_direita():
-    global orientacao
-    if orientacao == 'norte':
-        orientacao = 'oeste'
-    elif orientacao == 'oeste':
-        orientacao = 'sul'
-    elif orientacao == 'sul':
-        orientacao = 'leste'
-    elif orientacao == 'leste':
-        orientacao = 'norte'
-
-girar_direita()
+def mover(direção, posição_atual):
+    if direção == 'frente':
+        posição_atual[0] -= 1
+        
+    elif direção == 'direita':
+        posição_atual[1] += 1
+    
+    elif direção == 'esquerda':
+        posição_atual[1] -= 1
+    
+    elif direção == 'tras':
+        posição_atual[0] += 1
+        
+        
+    if posição_atual[0] == -1:
+        posição_atual[0] += 1
+    if posição_atual[1] == -1:
+        posição_atual[1] += 1
+        
+    
+    return posição_atual
 
 
+teste = ['frente',  #0, 1
+         'frente',  #0, 1
+         'direita', #0, 2
+         'tras',    #1, 2
+         'tras',    #2, 2
+         'esquerda', #2, 1
+         'frente',  #1, 1
+         'esquerda']#1, 0
+aspirador = posicao
+
+
+for i, pos in enumerate(teste):
+    aspirador = mover(pos, aspirador)
+    print(aspirador)
 
