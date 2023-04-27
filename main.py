d4604b44984b91555c25e806383af7f8e62b4b2d
felpk <mfrzbr@gmail.com>
2023-04-27 09:07:14 -0300

    Implementação eficiência

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ sujo = 1
 limpo = 0
 
 posicao = [1, 1]    #posicao inicial
-posicao_atual = posicao #posicao atual
+#posicao_atual = posicao #posicao atual
+
+qtd_movimentos = 0
+qtd_aspirar = 0
+
 table = [
-    [(0, 0), sujo],  [(0, 1), sujo],  [(0, 2), limpo],
-    [(1, 0), limpo], [(1, 1), limpo], [(1, 2), sujo],
-    [(2, 0), sujo],  [(2, 1), limpo], [(2, 2), sujo]
+    [[(0, 0), sujo],  [(0, 1), sujo],  [(0, 2), limpo]],
+    [[(1, 0), limpo], [(1, 1), sujo], [(1, 2), sujo]],
+    [[(2, 0), sujo],  [(2, 1), limpo], [(2, 2), sujo]]
 ]
+
 
 
 def mover(direção, posição_atual):
@@ -28,47 +33,64 @@ def mover(direção, posição_atual):
         posição_atual[0] += 1
     if posição_atual[1] == -1:
         posição_atual[1] += 1
-        
     
     return posição_atual
 
-def aspirar():  # Funcao Aspirar Funcionando
-    global i
-    global j 
-    i = posicao_atual[0]
-    j = posicao_atual[1]
 
-    if table[i][j] == limpo:
-        return(f'Espaço [{i, j}] já está limpo!\n')
 
-    elif table[i][j] == sujo:
-        table[i][j] = limpo
-        return(f'Espaço [{i, j}] limpo!\n')
+def aspirar(posição_atual, table):
 
-# for i, pos in enumerate(teste):
-#     aspirador = mover(pos, aspirador)
-#     print(aspirador)
+    i, j = posição_atual[0], posição_atual[1]
 
-def imprimir_ambiente():    # Imprimir matriz ambiente onde 0 é limpo e 1 é sujo Funcionando
-    print()
-    for i in range(3):
-        for j in range(3):
-            index = i * 3 + j
-            print(table[index][1], end=' ')
-        print()
+    if table[i][j][1] == 0:
+        print(f'Espaço [{i, j}] já está limpo!\n')
+
+    elif table[i][j][1] == 1:
+        table[i][j][1] = 0
+        print(f'Espaço [{i, j}] Aspirado!\n')
+    
+    return table
+
+
+def imprimir_ambiente(table):    # Imprimir matriz ambiente onde 0 é limpo e 1 é sujo Funcionando
+    for i in range(len(table)):
+        print(table[i])
     print()
 
 
-def busca_ambiente():  # Verifica redondezas do agente para identificar sujeiras 
-    ####
-    return 0
 
-print("\nASPIRADOR")
+def encontrar_sujos(table):
+    sujos = []
+    for i in range(len(table)):
+        for j in range(len(table[i])):
+            if table[i][j][1] == 1:
+                sujos.append((i, j))
+    return sujos
 
-imprimir_ambiente()
 
-print("\nPosicao atual: ", busca_ambiente())
 
-imprimir_ambiente()
 
-print(table)
+teste = ['norte',  #0, 1
+         'norte',  #0, 1
+         'direita', #0, 2
+         'sul',    #1, 2
+         'sul',    #2, 2
+         'esquerda', #2, 1
+         'norte',  #1, 1
+         'esquerda']#1, 0
+
+
+imprimir_ambiente(table)
+
+for i, pos in enumerate(teste):
+    posicao = mover(pos, posicao)
+    qtd_movimentos += 1
+    print(posicao)
+    table = aspirar(posicao, table)
+    qtd_aspirar += 1
+
+imprimir_ambiente(table)
+
+eficiencia =  (f'{(qtd_movimentos/qtd_aspirar)*100}%')
+
+print(f'Movimentos/Aspirações: {qtd_movimentos} / {qtd_aspirar} \n\nEficiência de {eficiencia}.\n ')
