32cb8cf7fefdcc8ed9e8ed1b0d09f0dd8f9657d3
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2023-04-28 21:30:29 -0300

    Implementada função mostrar_andando

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 sujo = 1
 limpo = 0
 
@@ -13,33 +11,28 @@ table = [
     [[(0, 0), sujo],  [(0, 1), sujo],  [(0, 2), limpo]],
     [[(1, 0), limpo], [(1, 1), sujo], [(1, 2), sujo]],
     [[(2, 0), sujo],  [(2, 1), limpo], [(2, 2), sujo]]
-    [[(0, 0), sujo],  [(0, 1), sujo],  [(0, 2), limpo]],
-    [[(1, 0), limpo], [(1, 1), sujo], [(1, 2), sujo]],
-    [[(2, 0), sujo],  [(2, 1), limpo], [(2, 2), sujo]]
 ]
 
 
 
-def mover(direção, posição_atual):
+def mover(direção, posição_atual, table):
     global qtd_movimentos
-
-    #posicao_atual = posicao_atual.copy()
 
     if direção == 'norte':
         posição_atual[0] -= 1
-        print(f'moveu-se para o norte ^', end='')
+        print(f'moveu-se para o norte ^')#, end='')
         
     elif direção == 'oeste':
         posição_atual[1] += 1
-        print(f'moveu-se para a direita >', end='')
+        print(f'moveu-se para a direita >')#, end='')
     
     elif direção == 'leste':
         posição_atual[1] -= 1
-        print(f'moveu-se para a esquerda <', end='')
+        print(f'moveu-se para a esquerda <')#, end='')
     
     elif direção == 'sul':
         posição_atual[0] += 1
-        print(f'moveu-se para o sul v', end='')
+        print(f'moveu-se para o sul v')#, end='')
         
     # Verifica se a posição atual ultrapassa os limites da matriz
     if posição_atual[0] == -1:
@@ -48,7 +41,8 @@ def mover(direção, posição_atual):
         posição_atual[1] += 1
     
     qtd_movimentos = qtd_movimentos + 1
-    print(f'{posição_atual}')
+    mostar_andando(table, posição_atual)
+    #print(f'{posição_atual}\n')
     return posição_atual
 
 
@@ -61,6 +55,13 @@ def imprimir_ambiente(table):    # Imprimir matriz ambiente onde 0 é limpo e 1 
 
 
 
+def mostar_andando(table, posição_atual):
+    i, j = posição_atual
+    table[i][j].append('x')
+    imprimir_ambiente(table)
+    table[i][j].remove('x')
+
+
 def buscar_sujos(table):
     global sujos
     sujos = []
@@ -71,26 +72,26 @@ def buscar_sujos(table):
     return sujos
 
 
-def mover_para(posicao_atual, posicao_destino):
+def mover_para(posicao_atual, posicao_destino, table):
     direcao_norte = 'norte'
     direcao_sul = 'sul'
-    direcao_esquerda = 'esquerda'
-    direcao_direita = 'direita'
+    direcao_esquerda = 'leste'
+    direcao_direita = 'oeste'
     nova_posicao = posicao_atual.copy()
 
     # Movendo o aspirador norte/sul para chegar na posição destino
     while nova_posicao[0] != posicao_destino[0]:
         if nova_posicao[0] < posicao_destino[0]:
-            nova_posicao = mover(direcao_sul, nova_posicao)
+            nova_posicao = mover(direcao_sul, nova_posicao, table)
         else:
-            nova_posicao = mover(direcao_norte, nova_posicao)
+            nova_posicao = mover(direcao_norte, nova_posicao, table)
 
     # Movendo o aspirador na horizontal para chegar na posição destino
     while nova_posicao[1] != posicao_destino[1]:
         if nova_posicao[1] < posicao_destino[1]:
-            nova_posicao = mover(direcao_direita, nova_posicao)
+            nova_posicao = mover(direcao_direita, nova_posicao, table)
         else:
-            nova_posicao = mover(direcao_esquerda, nova_posicao)
+            nova_posicao = mover(direcao_esquerda, nova_posicao, table)
 
     return nova_posicao
 
@@ -118,7 +119,7 @@ def aspirar(posição_atual, table):
 
     return table
 
-def encontrar_aspirar_sujos(table, sujos):
+def encontrar_aspirar_sujos(table):
     posicao_atual = [1, 1]
     global qtd_aspirar
     global qtd_movimentos
@@ -126,9 +127,8 @@ def encontrar_aspirar_sujos(table, sujos):
     for i in range(len(table)):           # Itera pelos elementos da table
 
         for j in range(len(table[i])):      # Itera dentro de cada elemento da table
-            if table[i][j][1] == 1:         # Se quadrado atual == sujo
-                #posicao_atual = [i, j]                                  
-                posicao_atual = mover_para(posicao_atual, [i, j])
+            if table[i][j][1] == 1:         # Se quadrado atual == sujo                                 
+                posicao_atual = mover_para(posicao_atual, [i, j], table)
                 print("Posição atual:", posicao_atual)
                 table = aspirar(posicao_atual, table)
     return qtd_aspirar
@@ -137,7 +137,7 @@ def encontrar_aspirar_sujos(table, sujos):
 
 imprimir_ambiente(table)
 sujos = buscar_sujos(table)
-encontrar_aspirar_sujos(table, sujos)
+encontrar_aspirar_sujos(table)
 imprimir_ambiente(table)
 
 print(eficiencia(qtd_movimentos, qtd_aspirar))
